ccdbe1ee283bc6feb471f7ac505f86f77eb07072
Mario Barbosa <mbarbosa@procenge.com.br>
2019-09-18 18:20:01 -0300

    adding Algorithmia API(WIP 1)

--- robots/text.js
+++ robots/text.js
@@ -1,10 +1,17 @@
+const algorithmia = require('algorithmia')
+
 function robot(content) {
     fetchContentFromWikipedia(content)
     // sanitizeContent(content)
     // breakContentIntoSentences(content)
 
     function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia('')
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
 
+        console.log(wikipediaContent)
     }
 }
 
