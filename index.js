d14b75478a78aa533241042f9b7e275f4a42d732
Mario Barbosa <mbarbosa@procenge.com.br>
2019-09-18 17:30:20 -0300

    Creating text robot (WIP 1)

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const redline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
 
-    function start() {
-        const content = {}
+function start() {
+    const content = {}
 
-        content.searchTerm = askAndReturnSearchTerm()
-        content.prefix = askAndReturnPrefix()
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
-        function askAndReturnSearchTerm() {
-            return redline.question('Type a Wikipedia search term: ')
-        }
+    robots.text(content)
+
+    function askAndReturnSearchTerm() {
+        return redline.question('Type a Wikipedia search term: ')
+    }
 
     function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = redline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixIndex = redline.keyInSelect(prefixes, `Choose one prefix for the term: '${content.searchTerm}'` )
         const selectedPrefixText = prefixes[selectedPrefixIndex]
 
         return selectedPrefixText
     }
 
-    console.log(content)
+    console.log('Final search term: '+content.prefix + ' ' + content.searchTerm +)
 }
 
 start()
