28cd011b62f6498808bec28908de6b0fa116c08c
Mario Barbosa <mbarbosa@procenge.com.br>
2019-09-18 17:33:48 -0300

    Creating text robot (WIP 1)

--- robots/text.js
+++ robots/text.js
@@ -1,7 +1,11 @@
 function robot(content) {
     fetchContentFromWikipedia(content)
-    sanitizeContent(content)
-    breakContentIntoSentences(content)
+    // sanitizeContent(content)
+    // breakContentIntoSentences(content)
+
+    function fetchContentFromWikipedia(content) {
+
+    }
 }
 
 module.exports = robot
